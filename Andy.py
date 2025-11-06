46a382f15b53ad2c8811a1e3b94c4a53b2545b5c
Gucci HO <tho22@student.aau.dk>
2025-11-06 13:41:39 +0100

    Setup

--- Andy.py
+++ Andy.py
@@ -1,8 +1,18 @@
 import joblib
 import pandas as pd
+import numpy as np
 
-# Load trained model/imputer
+# Load trained imputer
 imputer = joblib.load("final_imputer.pkl")
 
-# Impute missing values in new data
-new_data_imputed = imputer.transform(input_data[num_cols_no_gender])
+input_df = # et eller andet fra Unity converted til en pandas Dataframe for compatibility.
+
+# Drop gender fordi det ikke skal bruges af modellen, align columns så features er i den rigtige rækkefølge
+numeric_cols_no_gender = [col for col in imputer.feature_names_in_]
+input_df_aligned = input_df[numeric_cols_no_gender]
+
+# Impute missing values
+new_data_imputed = imputer.transform(input_df_aligned)
+
+print(new_data_imputed)
+
