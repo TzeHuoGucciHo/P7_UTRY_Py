c0d84df85d9a7f8db0526b20b50c106886d2880d
Goodest person <mflens22@student.aau.dk>
2025-11-10 11:41:17 +0100

    Landing page
    Unity ui zalando page til utry page

---
+++ Unity UI/Python UI intergration/Assets/Scripts/WebLandingPage/Scenecontroller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenecontroller : MonoBehaviour
+{
+    public void sceneChange()
+    {
+        SceneManager.LoadScene("UtryMain");
+    }
+    
+    
+}
