11fe3431e5cdfb5b3f2d27af5a5da6b55784da48
Goodest person <mflens22@student.aau.dk>
2025-11-10 14:18:37 +0100

    INputfield

--- Unity UI/Python UI intergration/Assets/Scripts/displayNumbers.cs
+++ Unity UI/Python UI intergration/Assets/Scripts/displayNumbers.cs
@@ -1,10 +1,14 @@
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class displayNumbers : MonoBehaviour
 {
-    public InputField height;
+    public TMP_InputField height;
+    public TMP_InputField shoulderWidth;
+    public TMP_InputField waistWidth;
+    public TMP_InputField hipWidth;
 
     public void getNumbers()
     {
@@ -20,7 +24,12 @@ public class displayNumbers : MonoBehaviour
         Process p = Process.Start(psi);
         string output = p.StandardOutput.ReadToEnd();
         p.WaitForExit();
+        string[] pairs = output.Split(',');
 
-        height.text = output.Trim();
+        height.text = pairs[0];
+        shoulderWidth.text = pairs[1];
+        waistWidth.text = pairs[2];
+        hipWidth.text = pairs[3];
+
     }
 }
