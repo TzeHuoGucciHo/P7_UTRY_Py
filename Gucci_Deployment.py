2b2f021f877b4fd7a5e6deb7a3ef099ab9682670
Gucci HO <tho22@student.aau.dk>
2025-12-10 09:32:01 +0100

    Deployment script

--- Gucci_Deployment.py
+++ Gucci_Deployment.py
@@ -1,15 +1,13 @@
+
 import joblib
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import PowerTransformer
 
-# Load saved models and feature names
 imputer = joblib.load("final_imputer.pkl")
-# Use the feature names from the imputer to define the order of columns
 numeric_cols_no_gender = list(imputer.feature_names_in_)
 transformer: PowerTransformer = joblib.load("transformer.pkl")
 
-# --- NEW INPUT DATA (In CM) ---
 data = {
     "Gender": [1.0],
     "Age": [23.0],
@@ -25,7 +23,6 @@ data = {
     "LegLength": [np.nan],
     "TotalHeight": [190.0]
 }
-
 true_val = {
     "Gender": 1.0,
     "Age": 23.0,
@@ -46,45 +43,21 @@ def get_missing_mask(df, cols):
     return df[cols].isna()
 
 input_df = pd.DataFrame(data)
-
 true_val = pd.DataFrame([true_val])
-
-all_numeric_cols = [col for col in input_df.columns if input_df[col].dtype != 'object']
-# ----------------------------------------------------
 
 mask_original = get_missing_mask(input_df, numeric_cols_no_gender)
 input_scaled = transformer.transform(input_df[numeric_cols_no_gender])
-
 imputed_scaled_array = imputer.transform(input_scaled)
 imputed_original_scale_array = transformer.inverse_transform(imputed_scaled_array)
-
-imputed_original_scale_df = pd.DataFrame(
-    imputed_original_scale_array,
-    columns=numeric_cols_no_gender,
-    index=input_df.index
-)
+imputed_original_scale_df = pd.DataFrame(imputed_original_scale_array, columns=numeric_cols_no_gender, index=input_df.index)
 
 final_result_df = input_df.copy()
 for col in numeric_cols_no_gender:
     final_result_df.loc[mask_original[col], col] = imputed_original_scale_df[col]
 
-
-if input_df['Age'][0] > 18 and input_df['TotalHeight'][0] > 180:
-    waist_est = input_df['TotalHeight'][0] * 0.54
-    hips_est = input_df['TotalHeight'][0] * 0.60
-    if abs(final_result_df['Waist'][0] - waist_est) > 10:
-        final_result_df['Waist'][0] = waist_est
-    if abs(final_result_df['Hips'][0] - hips_est) > 10:
-        final_result_df['Hips'][0] = hips_est
-
-
-
-print("--- Input Data with Missing Values (Converted to CM) ---")
+print("--- Input Data ---")
 print(input_df.to_string())
-
-print("\n--- Imputed Data (Only imputed entries are in CM scale) ---")
+print("\n--- Imputed Data ---")
 print(final_result_df.to_string())
-
 print("\n--- True Val ---")
 print(true_val.to_string())
-
